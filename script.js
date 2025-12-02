d32ea9af55eb6147c7d82cf6cdfa1ed4ac3dcd4c
Ismail Hossain <mdismailtanvir6@gmail.com>
2025-12-02 08:47:02 +0600

    read file practice is completed

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ console.log("Hello from Node!");
 
 
 const fs = require("fs");
-const readFiles = fs.readFileSync("text.txt", "utf-8");
-console.log(readFiles);
+const readFiles1 = fs.readFileSync("text.txt", "utf-8");
+console.log(readFiles1);
+const readFiles2 = fs.readFileSync("text2.txt", "utf-8");
+console.log(readFiles2);
+const readFiles3 = fs.readFileSync("text3.txt", "utf-8");
+console.log(readFiles3);
