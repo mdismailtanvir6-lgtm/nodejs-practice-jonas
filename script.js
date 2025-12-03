d37e66aa04b75114b4f45eec4bc4ac0ea1708e26
Ismail Hossain <mdismailtanvir6@gmail.com>
2025-12-04 04:42:38 +0600

    read file by asynchronous function

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const hello = "hello world !";
-console.log(hello);
-console.log("Hello from Node!");
-
 // ======= file system from node ==========
+const { log } = require("console");
 const fs = require("fs");
 
 // =========== read files =======
@@ -11,24 +8,33 @@ const readFiles2 = fs.readFileSync("text2.txt", "utf-8");
 const readFiles3 = fs.readFileSync("text3.txt", "utf-8");
 // console.log(readFiles1, readFiles2, readFiles3);
 
+// =========== read file synchronously : 1 ===========
+const readFilsSync = fs.readFileSync("text.txt", "utf-8");
+console.log(readFilsSync);
+console.log("This is synchronous !")
+
+// =========== read file asynchronously : 1 ===========
+fs.readFile("text2.txt", "utf-8", (err, data)=>{
+    console.log(data);
+});
+console.log("This is asynchronous !");
+
 
 // ============= write file here ============
 // ==== txt file ===
 const fileName = `writeFile1`;
 const fileContent = `This is a text of ${fileName} \n${new Date().toDateString()}`;
 fs.writeFileSync(`./${fileName}.txt`, fileContent);
-console.log(fileName + " has been created");
+// console.log(fileName + " has been created");
 
 // ==== html file ===
 const fileName2 = `writeFile2`;
 const fileContent2 = `<p>This is a text of ${fileName2}</p>\n<p>${new Date().toDateString()}</p>`;
 fs.writeFileSync(`./${fileName2}.html`, fileContent2);
-console.log(fileName2 + " has been created");
-
+// console.log(fileName2 + " has been created");
 
 // ==== js file ===
 const fileName3 = `writeFile3`;
 const fileContent3 = `const fileText = "here is text of file 03"`;
 fs.writeFileSync(`./${fileName3}.js`, fileContent3);
-console.log(fileName3 + " has been created");
-
+// console.log(fileName3 + " has been created");
