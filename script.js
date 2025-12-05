132a423ad3c180ffe14ba35c7da25fdbc1f50065
Ismail Hossain <mdismailtanvir6@gmail.com>
2025-12-05 08:41:31 +0600

    read and write file with asynchronous way

--- script.js
+++ script.js
@@ -19,47 +19,6 @@ const readFilsSync = fs.readFileSync("text.txt", "utf-8");
 // });
 // console.log("This is asynchronous !");
 
-// =========== read file asynchronously with callback function ===========
-fs.readFile("text.txt", "utf-8", (err, data1) => {
-  console.log(data1);
-
-  fs.readFile("text2.txt", "utf-8", (err, data2) => {
-    console.log(data2);
-
-    fs.readFile("text3.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-
-      fs.readFile("text4.txt", "utf-8", (err, data4) => {
-        console.log(data4);
-
-        fs.readFile("text5.txt", "utf-8", (err, data5) => {
-          console.log(data5);
-
-          fs.readFile("text6.txt", "utf-8", (err, data6) => {
-            console.log(data6);
-
-            fs.readFile("text7.txt", "utf-8", (err, data7) => {
-              console.log(data7);
-
-              fs.readFile("text8.txt", "utf-8", (err, data8) => {
-                console.log(data8);
-
-                fs.readFile("text9.txt", "utf-8", (err, data9) => {
-                  console.log(data9);
-
-                  fs.readFile("text10.txt", "utf-8", (err, data10) => {
-                    console.log(data10);
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-});
-
 // ============= write file here ============
 // ==== txt file ===
 const fileName = `writeFile1`;
@@ -78,3 +37,32 @@ const fileName3 = `writeFile3`;
 const fileContent3 = `const fileText = "here is text of file 03"`;
 fs.writeFileSync(`./${fileName3}.js`, fileContent3);
 // console.log(fileName3 + " has been created");
+
+
+
+
+// =========== read file and write file with asynchronous way ===========
+fs.readFile("text.txt", "utf-8", (err, data1) => {
+  err && console.log("error from data1");
+
+  console.log(data1);
+
+  fs.readFile("text2.txt", "utf-8", (err, data2) => {
+    console.log(data2);
+
+    fs.readFile("text3.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./final.txt", data3, "utf-8", (err) => {
+        err
+          ? console.log("error from data3 write file ")
+          : console.log("Your file has been written !");
+
+        fs.readFile("final.txt", "utf-8", (err, data4) => {
+          console.log(data4);
+        });
+      });
+    });
+  });
+});
+console.log("hello async");
