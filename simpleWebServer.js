257dcc2e2cc2af8988be6c9a1939b61111bb0add
Ismail Hossain <mdismailtanvir6@gmail.com>
2025-12-19 01:00:33 +0600

    22 - Setting up Prettier in VS Code is done

--- simpleWebServer.js
+++ simpleWebServer.js
@@ -1,38 +1,35 @@
 // ========== importing http and url from node ======
-const fs = require("fs");
-const http = require("http");
-const path = require("path");
-const url = require("url");
-const slugify = require("slugify");
-const replaceTemplate = require("./module/replaceTemplate");
-const { log } = require("console");
-
-
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./module/replaceTemplate');
+const { log } = require('console');
 
 ///////////////////////////////////////////
 // ===== Reading Templates & Data ========
 const template_overview = fs.readFileSync(
   `${__dirname}/template/template_overview.html`,
-  "utf-8"
+  'utf-8'
 );
 
 const template_product = fs.readFileSync(
   `${__dirname}/template/template_product.html`,
-  "utf-8"
+  'utf-8'
 );
 
 const product_card = fs.readFileSync(
   `${__dirname}/template/product_card.html`,
-  "utf-8"
+  'utf-8'
 );
 
-const api = fs.readFileSync(`${__dirname}/Data/data.json`, "utf-8");
+const api = fs.readFileSync(`${__dirname}/Data/data.json`, 'utf-8');
 const dataAPI = JSON.parse(api);
 
 // ======== making slug ===========
-const slugs = dataAPI.map((item) => slugify(item.productName, { lower : true}));
+const slugs = dataAPI.map(item => slugify(item.productName, { lower: true }));
 console.log(slugs);
-
 
 ///////////////////////////////////////////
 // ===== Creating Server =========
@@ -40,12 +37,12 @@ const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
 
   // ========= OVERVIEW PAGE ========
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     const cardHtml = dataAPI
-      .map((item) => replaceTemplate(product_card, item))
-      .join("");
+      .map(item => replaceTemplate(product_card, item))
+      .join('');
 
     const output = template_overview.replace(/{%PRODUCT_CARDS%}/g, cardHtml);
 
@@ -54,12 +51,12 @@ const server = http.createServer((req, res) => {
 
   // ========= PRODUCT  PAGE ========
   else if (pathname === `/product`) {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
     const product = dataAPI[query.id];
 
     if (!product) {
-      res.end("<h1>Product Not Found!</h1>");
+      res.end('<h1>Product Not Found!</h1>');
       return;
     }
 
@@ -68,26 +65,28 @@ const server = http.createServer((req, res) => {
   }
 
   // ========= API ROUTE ========
-  else if (pathname === "/api") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+  else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(api);
   }
 
   // ========= 404 PAGE ========
   else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>Oops! Page Not Found</h1>");
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Oops! Page Not Found</h1>');
   }
 });
 
 ///////////////////////////////////////////
 // ===== Server Listen ========
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Server is running on port 8000...");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server is running on port 8000...');
 });
 
+console.log('hello');
+console.log('hello');
 
+console.log(hello);
 
-
-
+console.log('hello');
 
